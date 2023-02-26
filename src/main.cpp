89f3dbddf5146d83cecd5900e12fa8fc00af5c71
M-Trevizo <michael.trevizo93@gmail.com>
2023-02-25 23:16:34 -0800

    Start writing main function.

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <chip8.h>
+
+using namespace std;
+using namespace CPU;
+
+int main(int argc, char** argv) {
+
+    // Check arguments for single filepath
+    if(argc > 2) {
+        cout << "Too many arguments given." << endl;
+        return 1;
+    }
+
+    string path = argv[1];
+    ifstream stream;
+    stream.open(path, ios::binary);
+
+    if(!stream.is_open()) {
+        cout << "Unable to open file." << endl;
+        return 1;
+    }
+    
+
+    stream.close();
+    
+}
